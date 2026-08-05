45175cb7e7460c581ea9b29413c6ed9918b866ff
pengkj041020 <pengkj2023@shanghaitech.edu.cn>
2026-08-05 09:32:49 +0800

    路径更正

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,5 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  // 重要：因为你的仓库名是 my-world，访问地址是 /my-world/
-  // 所以 base 必须设置为 '/my-world/'
-  base: '/my-world/',
+  base: '/my-world/',  // 关键：必须与你的仓库名一致
 })
